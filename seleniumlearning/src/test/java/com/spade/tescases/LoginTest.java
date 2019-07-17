0118d9b8c8313c015c4181a63f551ed61fe13685
Ankit Samant <ankit@spadeinfotech.net>
2019-07-17 13:05:26 +0530

    1.title and url compare 2.implicit and explicit wait

--- seleniumlearning/src/test/java/com/spade/tescases/LoginTest.java
+++ seleniumlearning/src/test/java/com/spade/tescases/LoginTest.java
@@ -2,9 +2,17 @@ package com.spade.tescases;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 
@@ -21,21 +29,26 @@ public class LoginTest {
 		  driver=new ChromeDriver();
 		  driver.manage().window().maximize();
 		  driver.manage().deleteAllCookies();
+		  //implicit wait
+		  driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		  driver.get("http://192.168.11.125:8039/MotorInsuranceApp/#/Login");
+		  System.out.println();
 		    
 	  }
 
 	  
 	  @AfterTest
-	  public void closebrowser() {
-		 // driver.quit();
+	  public void closebrowser() throws InterruptedException {
+		
+		  Thread.sleep(5000);
+		  driver.quit();
 		 
 	  }
 
 	
 	
 	
-  @Test
+  @Test(priority=1)
   public void loginintotheApplication() {
 	  
 	  driver.findElement(By.id("username")).clear();
@@ -47,15 +60,48 @@ public class LoginTest {
 	  
   }
   
-  @Test
-  public void verify_homepagetitle() {
+  @Test(priority=2)
+  public void  verify_homepagetitle() {
+	String Actualtitle=  driver.getTitle();
+	String expected="Motor Claim";
 	  
+//Assert.assertEquals(Actualtitle, expected);
+Assert.assertEquals(Actualtitle, expected, "title mismatch");
 	  
+System.out.println("title matched");
+
+if(Actualtitle.equals(expected)) {
+	System.out.println("matched");
+}else {
+	System.out.println("Not matched");
+	
+}
+ }
+ 
+  
+  @Test(priority=3)
+  public void verify_Homepageurl() throws InterruptedException {
+
+	  Thread.sleep(2000);
 	  
-  }
-  @Test
-  public void verify_Homepageurl() {
+	  //explicit wait
+	//  WebDriverWait wait=new WebDriverWait(driver, 2);
+	 // wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("homepage")));
 	  
+      String Actualurl=driver.getCurrentUrl();
+
+	  String expectedurl="http://192.168.11.125:8039/MotorInsuranceApp/#/Home";
+	//Assert.assertEquals(Actualtitle, expected);
+	  Assert.assertEquals(Actualurl, expectedurl, "title mismatch");
+	  	  
+	  System.out.println("title matched");
+
+	  //alternate way to compare
+	  if(Actualurl.equals(expectedurl)) {
+	  	
+	  }else {
+	  	
+	  }  
 	  
 	  
   }
